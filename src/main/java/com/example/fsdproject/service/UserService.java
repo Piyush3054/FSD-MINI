3ce8ee8a1c745a38d442f478a9ea26882fe39839
Piyush3054 <makwanpiyush3054@gmail.com>
2024-02-15 17:06:41 +0530

    src added

---
+++ src/main/java/com/example/fsdproject/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.fsdproject.service;
+
+import com.example.fsdproject.entity.User;
+import com.example.fsdproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findByUsername(String username)
+    {
+        return userRepository.findByUsername(username);
+    }
+    public User findByEmail(String email)
+    {
+        return userRepository.findByUsername(email);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+}
