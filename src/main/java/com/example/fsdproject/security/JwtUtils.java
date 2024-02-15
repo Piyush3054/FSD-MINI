53c786a4d02771f2665f479752d44e5ce31fe4ed
Piyush3054 <makwanpiyush3054@gmail.com>
2024-02-15 18:38:26 +0530

    op

--- src/main/java/com/example/fsdproject/security/JwtUtils.java
+++ src/main/java/com/example/fsdproject/security/JwtUtils.java
@@ -1,5 +1,6 @@
 package com.example.fsdproject.security;
 
+import com.example.fsdproject.entity.Admin;
 import com.example.fsdproject.entity.User;
 
 import java.util.Date;
@@ -8,8 +9,12 @@ public class JwtUtils {
 
     private static final long EXPIRATION_TIME = 86400000; // 1 day in milliseconds
 
-    public static String generateToken(User user) {
+    public static String generateUserToken(User user) {
         // Creating a simple, unencrypted token string
         return user.getUsername() +"|"+ user.getEmail()+"|" + (System.currentTimeMillis() + EXPIRATION_TIME);
+    }
+    public static String generateAdminToken(Admin admin) {
+        // Creating a simple, unencrypted token string
+        return admin.getUsername() +"|"+ admin.getEmail()+"|" + (System.currentTimeMillis() + EXPIRATION_TIME);
     }
 }
